5b5bf0efb0e2250b2eb2450214a2961e68b30eb4
Victor Mireyev <victor@opennodecloud.com>
2017-09-07 14:27:49 +0300

    Fix SLURM user role synchronization.

--- src/waldur_slurm/handlers.py
+++ src/waldur_slurm/handlers.py
@@ -14,17 +14,17 @@ def process_user_deletion(sender, instance, **kwargs):
     tasks.delete_user.delay(core_utils.serialize_instance(instance))
 
 
-def process_role_granted(sender, instance, **kwargs):
+def process_role_granted(sender, structure, user, role, **kwargs):
     try:
-        freeipa_profile = freeipa_models.Profile.objects.get(user=instance)
+        freeipa_profile = freeipa_models.Profile.objects.get(user=user)
         tasks.add_user.delay(core_utils.serialize_instance(freeipa_profile))
     except freeipa_models.Profile.DoesNotExist:
         pass
 
 
-def process_role_revoked(sender, instance, **kwargs):
+def process_role_revoked(sender, structure, user, role, **kwargs):
     try:
-        freeipa_profile = freeipa_models.Profile.objects.get(user=instance)
+        freeipa_profile = freeipa_models.Profile.objects.get(user=user)
         tasks.delete_user.delay(core_utils.serialize_instance(freeipa_profile))
     except freeipa_models.Profile.DoesNotExist:
         pass
