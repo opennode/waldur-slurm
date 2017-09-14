5cc7c5d2b29500ab5ad5732923576cfd0bc2edaa
Victor Mireyev <victor@opennodecloud.com>
2017-09-14 17:17:24 +0300

    Aggregate allocation quotas usage by project and customer [WAL-1147] Add CPU, GPU and RAM usage quota fields for project and customer.

--- src/waldur_slurm/apps.py
+++ src/waldur_slurm/apps.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 from django.db.models import signals
 
+from nodeconductor.quotas.fields import QuotaField
+
 
 class SlurmConfig(AppConfig):
     name = 'waldur_slurm'
@@ -16,7 +18,7 @@ class SlurmConfig(AppConfig):
         from waldur_freeipa import models as freeipa_models
 
         from .backend import SlurmBackend
-        from . import handlers
+        from . import handlers, models, utils
 
         SupportedServices.register_backend(SlurmBackend)
 
@@ -45,3 +47,20 @@ class SlurmConfig(AppConfig):
                 sender=model,
                 dispatch_uid='waldur_slurm.handlers.process_role_revoked.%s' % model.__class__,
             )
+
+        for quota in utils.QUOTA_NAMES:
+            structure_models.Customer.add_quota_field(
+                name=quota,
+                quota_field=QuotaField(is_backend=True)
+            )
+
+            structure_models.Project.add_quota_field(
+                name=quota,
+                quota_field=QuotaField(is_backend=True)
+            )
+
+        signals.post_save.connect(
+            handlers.update_quotas_on_allocation_usage_update,
+            sender=models.Allocation,
+            dispatch_uid='waldur_slurm.handlers.update_quotas_on_allocation_usage_update',
+        )
---
+++ src/waldur_slurm/utils.py
@@ -0,0 +1,9 @@
+MAPPING = {
+    'cpu_usage': 'nc_cpu_usage',
+    'gpu_usage': 'nc_gpu_usage',
+    'ram_usage': 'nc_ram_usage',
+}
+
+FIELD_NAMES = MAPPING.keys()
+
+QUOTA_NAMES = MAPPING.values()
