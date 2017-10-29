a1aae72f61a2dc911d0b76d3951e603c8223c201
Jenkins <jenkins@opennodecloud.com>
2017-10-29 22:12:51 +0000

    Preparing new release: 0.3.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_requires = [
 
 setup(
     name='waldur-slurm',
-    version='0.2.7',
+    version='0.3.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
